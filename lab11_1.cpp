00408072d4ed79a8205b46dbf09c888f5f5fcae5
Kaofang <benyapa_kh@cmu.ac.th>
2021-01-15 15:20:20 +0700

    upd_fibonacci

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
-
+int fibonacci(int);
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int i){
+if(i==0) return 0;
+else if(i==1) return 1;
+else return fibonacci(i-1) + fibonacci(i-2);
 }
